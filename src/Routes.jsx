3573a67df97f0d271ae2383dc3524d491e9648eb
Rishaw Raj <rishawrajofficial75@gmail.com>
2025-12-19 02:15:22 +0530

    loading-hospital-commit

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import {useNavigate, useRoutes} from "react-router-dom"
-
+import { useNavigate, useRoutes } from "react-router-dom";
 
 //Auth context
 import { useAuth } from "./AuthContex";
@@ -18,78 +17,83 @@ import ViewPatient from "./components/dashboard/doctor/ViewPatient";
 import Prescription from "./components/dashboard/doctor/Prescription";
 import NotAuthorised from "./components/NotAuthorised";
 import NotFound from "./components/NotFound";
+import CircularProgress from '@mui/material/CircularProgress';
 
 const role = localStorage.getItem("role");
 
 const ProjectRoutes = () => {
-    const {currUser, setCurrUser} = useAuth();
-    const [role, setRole] = useState();
-    const navigate = useNavigate();
+  const { currUser, setCurrUser } = useAuth();
+  const [loading, setLoading] = useState(true);
+  const [role, setRole] = useState();
+  const navigate = useNavigate();
 
-    useEffect(()=> {
-        const userIdFromStorage = localStorage.getItem("userId");
-        const roleFromStorage = localStorage.getItem("role");
-        setRole(roleFromStorage);
-        if(!userIdFromStorage && !["/login", "/signup", "/"].includes(window.location.pathname)){
-            navigate("/");
-        }
+  useEffect(() => {
+    const userIdFromStorage = localStorage.getItem("userId");
+    const roleFromStorage = localStorage.getItem("role");
+    setRole(roleFromStorage);
+    setLoading(false);
+    if (
+      !userIdFromStorage &&
+      !["/login", "/signup", "/"].includes(window.location.pathname)
+    ) {
+      navigate("/");
+    }
 
-        if(userIdFromStorage && !currUser){
-            setCurrUser(userIdFromStorage);
-        }
+    if (userIdFromStorage && !currUser) {
+      setCurrUser(userIdFromStorage);
+    }
+  }, [currUser, setCurrUser]);
 
-
-    }, [currUser, setCurrUser]);
-
-    const element = useRoutes([
-        {
-            path: "/",
-            element: <Dashboard/>
-        },
-        {
-            path: "/register",
-            element: <Signup/>
-        },
-        {
-            path: "/login",
-            element: <Login/>
-        },
-        {
-            path: "/adminDashboard",
-            element: role==="admin" ? <AdminDashboard/> : <NotAuthorised/>
-        },
-        {
-            path: "/admin/hospital/:hospitalId",
-            element: role==="admin" ? <HospitalView/> : <NotAuthorised/>
-        },
-        {
-            path: "/admin/hospital",
-            element: role==="admin" ? <AddHospital/> : <NotAuthorised/>
-        },
-        {
-            path: "/admin/hospital/update/:hospitalId",
-            element: role==="admin" ? <UpdateHospital/> : <NotAuthorised/>
-        },
-        {
-            path: "/patientDashboard",
-            element: role==="patient" ? <PatientDashboard/> : <NotAuthorised/>
-        },
-        {
-            path: "/doctor/prescription/:patientId",
-            element: role==="doctor" ? <Prescription/> : <NotAuthorised/>
-        },
-        {
-            path: "/doctor/:patientId",
-            element: role==="doctor" ? <ViewPatient/> : <NotAuthorised/>
-        },
-        {
-            path: "*",
-            element: <NotFound/>
-        },
-        
-        
-    ])
-    return element;
-}
+  if (loading) {
+    return <CircularProgress size="3rem" />
+  }
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+    {
+      path: "/register",
+      element: <Signup />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/adminDashboard",
+      element: role === "admin" ? <AdminDashboard /> : <NotAuthorised />,
+    },
+    {
+      path: "/admin/hospital/:hospitalId",
+      element: role === "admin" ? <HospitalView /> : <NotAuthorised />,
+    },
+    {
+      path: "/admin/hospital",
+      element: role === "admin" ? <AddHospital /> : <NotAuthorised />,
+    },
+    {
+      path: "/admin/hospital/update/:hospitalId",
+      element: role === "admin" ? <UpdateHospital /> : <NotAuthorised />,
+    },
+    {
+      path: "/patientDashboard",
+      element: role === "patient" ? <PatientDashboard /> : <NotAuthorised />,
+    },
+    {
+      path: "/doctor/prescription/:patientId",
+      element: role === "doctor" ? <Prescription /> : <NotAuthorised />,
+    },
+    {
+      path: "/doctor/:patientId",
+      element: role === "doctor" ? <ViewPatient /> : <NotAuthorised />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+  return element;
+};
 
 export default ProjectRoutes;
